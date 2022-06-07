ce31d51242306868e7482ac04cb7f8afa9673c09
Gulshan Aggarwal <gulshanaggarwal333@gmail.com>
2022-06-07 16:59:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+
+const fetchData = async () => {
+    const response = await fetch("./posts.json");
+    const data = await response.json();
+    return data.posts;
+
+}
+
+
+// code execution start only after Dom Load
+window.addEventListener('load', async () => {
+
+    const container = document.querySelector('.container');
+    const data = await fetchData();
+    console.log(data);
+
+    data.forEach((post) => {
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('wrapper-instance');
+        wrapper.innerHTML = `
+        <div class="inner-box">
+        <h4>Title -</h4>
+        <p>${post.title}<p>
+        </div>
+        <div class="inner-box">
+        <h4>Description -</h4>
+        <p>${post.desc}<p>
+        </div>
+        <div class="inner-box">
+        <h4>Category -</h4>
+        <p>${post.category}<p>
+        </div>
+        <div class="inner-box">
+        <h4>Author -</h4>
+        <p>${post.author}<p>
+        </div>
+        `
+        container.appendChild(wrapper);
+
+    })
+
+
+})
